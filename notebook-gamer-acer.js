2831c881f6640093ba7cbc92045474cf3ce5c36d
Ricardo Costa Alves dos Santos <65743471+ricardo-cas@users.noreply.github.com>
2022-10-24 15:46:45 -0300

    Create notebook-gamer-acer.js

---
+++ notebook-gamer-acer.js
@@ -0,0 +1,394 @@
+[
+  {
+    index: 1,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN517-52 preta e vermelha 17.3", Intel Core i7 10750H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 144 Hz 1920x1080px Windows 11 Home',        
+    priceAmount: '7.999',
+    priceCents: '90',
+    TotalPrice: '7.999,90',
+    seller: 'Acer Brasil',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an517-52-preta-e-vermelha-173-intel-core-i7-10750h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-144-hz-1920x1080px-windows-11-home/p/mlb19036817?pdp_filters=category:mlb1652#searchvariation=mlb19036817&position=1&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 2,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN517-52 preta e vermelha 17.3", Intel Core i5 10300H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 144 Hz 1920x1080px Windows 11 Home',        
+    priceAmount: '5.999',
+    priceCents: '90',
+    TotalPrice: '5.999,90',
+    seller: 'Acer Brasil',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an517-52-preta-e-vermelha-173-intel-core-i5-10300h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-144-hz-1920x1080px-windows-11-home/p/mlb19070337?pdp_filters=category:mlb1652#searchvariation=mlb19070337&position=2&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 3,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN515-55 preta e vermelha 15.6", Intel Core i7 10750H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 144 Hz 1920x1080px Windows 11 Home',        
+    priceAmount: '8.999',
+    priceCents: '65',
+    TotalPrice: '8.999,65',
+    seller: 'Acer Brasil',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an515-55-preta-e-vermelha-156-intel-core-i7-10750h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-144-hz-1920x1080px-windows-11-home/p/mlb19563447?pdp_filters=category:mlb1652#searchvariation=mlb19563447&position=3&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 4,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN515-55 preta 15.6", Intel Core i5 10300H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 1920x1080px Windows 11 Home',
+    priceAmount: '5.999',
+    priceCents: '30',
+    TotalPrice: '5.999,30',
+    seller: 'Mercado Livre',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an515-55-preta-156-intel-core-i5-10300h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-1920x1080px-windows-11-home/p/mlb19125204?pdp_filters=category:mlb1652#searchvariation=mlb19125204&position=4&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 5,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN515-55 preta 15.6", Intel Core i5 10300H 8GB de RAM 1TB HDD 256GB SSD, NVIDIA GeForce GTX 1650 60 Hz 1920x1080px Windows 10 Home',
+    priceAmount: '5.161',
+    priceCents: '10',
+    TotalPrice: '5.161,10',
+    seller: 'Carrefour',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an515-55-preta-156-intel-core-i5-10300h-8gb-de-ram-1tb-hdd-256gb-ssd-nvidia-geforce-gtx-1650-60-hz-1920x1080px-windows-10-home/p/mlb19103954?pdp_filters=category:mlb1652#searchvariation=mlb19103954&position=5&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 6,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN515-44 preta e vermelha 15.6", AMD Ryzen 7 4800H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 144 Hz 1920x1080px Windows 11 Home',
+    priceAmount: '9.999',
+    priceCents: '90',
+    TotalPrice: '9.999,90',
+    seller: 'VIKING',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an515-44-preta-e-vermelha-156-amd-ryzen-7-4800h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-144-hz-1920x1080px-windows-11-home/p/mlb19167785?pdp_filters=category:mlb1652#searchvariation=mlb19167785&position=6&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 7,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN517-52 preta e vermelha 17.3", Intel Core i5 10300H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 144 Hz 1920x1080px Windows 10 Home',        
+    priceAmount: '5.700',
+    priceCents: '90',
+    TotalPrice: '5.700,90',
+    seller: 'FERNANDES2022',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an517-52-preta-e-vermelha-173-intel-core-i5-10300h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-144-hz-1920x1080px-windows-10-home/p/mlb18536488?pdp_filters=category:mlb1652#searchvariation=mlb18536488&position=7&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 8,
+    title: 'Notebook Nitro 5 Ci5 512gb 8gb Windows 11 An515-55-59t4 Acer',
+    priceAmount: '5.999',
+    priceCents: '30',
+    TotalPrice: '5.999,30',
+    link: 'https://produto.mercadolivre.com.br/mlb-2180769535-notebook-nitro-5-ci5-512gb-8gb-windows-11-an515-55-59t4-acer'
+  },
+  {
+    index: 9,
+    title: "Notebook Nitro 5 An517-52-77kz I7 W11 512gb Gtx 1650 17.3'",
+    priceAmount: '7.999',
+    priceCents: '90',
+    TotalPrice: '7.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2179703339-notebook-nitro-5-an517-52-77kz-i7-w11-512gb-gtx-1650-173'
+  },
+  {
+    index: 10,
+    title: 'Notebook Acer Nitro 5 8gb 1tb 256gb Ssd Tela 15,6 Windows11',
+    priceAmount: '5.799',
+    priceCents: '90',
+    TotalPrice: '5.799,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2612174523-notebook-acer-nitro-5-8gb-1tb-256gb-ssd-tela-156-windows11'
+  },
+  {
+    index: 11,
+    title: 'Notebook Acer Nitro 5 An515-55-79x0 Ci7 8gb 512gb Gtx 1650',
+    priceAmount: '8.999',
+    priceCents: '65',
+    TotalPrice: '8.999,65',
+    link: 'https://produto.mercadolivre.com.br/mlb-2679794069-notebook-acer-nitro-5-an515-55-79x0-ci7-8gb-512gb-gtx-1650'
+  },
+  {
+    index: 12,
+    title: 'Notebook Gamer Acer Nitro 5 An515-57-71m2 I7 8gb 1tb Ssd',
+    priceAmount: '5.999',
+    priceCents: '90',
+    TotalPrice: '5.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2864631150-notebook-gamer-acer-nitro-5-an515-57-71m2-i7-8gb-1tb-ssd'
+  },
+  {
+    index: 13,
+    title: 'Notebook Gamer Acer Nitro 5',
+    priceAmount: '6.300',
+    TotalPrice: '6.300,null',
+    link: 'https://produto.mercadolivre.com.br/mlb-2873935402-notebook-gamer-acer-nitro-5'
+  },
+  {
+    index: 14,
+    title: "Notebook Nitro 5 An517-52-50rs I5 W11 512gb Gtx 1650 17.3'",
+    priceAmount: '5.999',
+    priceCents: '90',
+    TotalPrice: '5.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2182098534-notebook-nitro-5-an517-52-50rs-i5-w11-512gb-gtx-1650-173'
+  },
+  {
+    index: 15,
+    title: 'Notebook Acer Nitro 5 An515-55-79x0 Ci7 8gb 512gb Gtx 1650',
+    priceAmount: '8.999',
+    priceCents: '90',
+    TotalPrice: '8.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2679783011-notebook-acer-nitro-5-an515-55-79x0-ci7-8gb-512gb-gtx-1650'
+  },
+  {
+    index: 16,
+    title: "Notebook Nitro 5 An515-55-59t4 Ci5 W11 512gb Gtx 1650 15.6'",
+    priceAmount: '5.999',
+    priceCents: '90',
+    TotalPrice: '5.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2179696975-notebook-nitro-5-an515-55-59t4-ci5-w11-512gb-gtx-1650-156'
+  },
+  {
+    index: 17,
+    title: 'Notebook Nitro 5 An517-54-56q0 I5 1tb Hdd 256gb Ssd Gtx1650',
+    priceAmount: '5.999',
+    priceCents: '18',
+    TotalPrice: '5.999,18',
+    link: 'https://produto.mercadolivre.com.br/mlb-2759516163-notebook-nitro-5-an517-54-56q0-i5-1tb-hdd-256gb-ssd-gtx1650'
+  },
+  {
+    index: 18,
+    title: 'Notebook Nitro 5 An517-54-56q0 I5 1tb Hdd 256gb Ssd Gtx1650',
+    priceAmount: '5.999',
+    priceCents: '90',
+    TotalPrice: '5.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2759490801-notebook-nitro-5-an517-54-56q0-i5-1tb-hdd-256gb-ssd-gtx1650'
+  },
+  {
+    index: 19,
+    title: "Notebook Gamer Nitro 5 An517-54-7204 I7 8gb 512gb Ssd 17.3'",
+    priceAmount: '6.999',
+    priceCents: '90',
+    TotalPrice: '6.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2811088200-notebook-gamer-nitro-5-an517-54-7204-i7-8gb-512gb-ssd-173'
+  },
+  {
+    index: 20,
+    title: 'Notebook Gamer Nitro An515-44-r4c8 R7 16gb 1tb Hd 256gb Ssd',
+    priceAmount: '6.999',
+    priceCents: '90',
+    TotalPrice: '6.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-1960710210-notebook-gamer-nitro-an515-44-r4c8-r7-16gb-1tb-hd-256gb-ssd'
+  },
+  {
+    index: 21,
+    title: 'Notebook Gamer Nitro 5 An517-54-72a6 Ci7 8gb 1tb Hdd 256gb',
+    priceAmount: '6.999',
+    priceCents: '90',
+    TotalPrice: '6.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2825295942-notebook-gamer-nitro-5-an517-54-72a6-ci7-8gb-1tb-hdd-256gb'
+  },
+  {
+    index: 22,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN515-44 preta 15.6", AMD Ryzen 7 4800H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 Ti 144 Hz 1920x1080px Windows 10 Home',
+    priceAmount: '6.599',
+    priceCents: '05',
+    TotalPrice: '6.599,05',
+    seller: 'ALISSON_EXPRESS',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an515-44-preta-156-amd-ryzen-7-4800h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-ti-144-hz-1920x1080px-windows-10-home/p/mlb18444745?pdp_filters=category:mlb1652#searchvariation=mlb18444745&position=8&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 23,
+    title: 'Notebook Gamer Nitro 5 An517-54-72a6 Ci7 8gb 1tb Hdd 256gb',
+    priceAmount: '6.999',
+    priceCents: '21',
+    TotalPrice: '6.999,21',
+    link: 'https://produto.mercadolivre.com.br/mlb-2825265748-notebook-gamer-nitro-5-an517-54-72a6-ci7-8gb-1tb-hdd-256gb'
+  },
+  {
+    index: 24,
+    title: "Notebook Gamer Nitro 5 An517-54-76eg I7 8gb 512gb Ssd 17.3'",
+    priceAmount: '6.999',
+    priceCents: '90',
+    TotalPrice: '6.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2825246100-notebook-gamer-nitro-5-an517-54-76eg-i7-8gb-512gb-ssd-173'
+  },
+  {
+    index: 25,
+    title: "Notebook Nitro 5 An517-52-50rs I5 W11 512gb Gtx 1650 17.3'",
+    priceAmount: '5.799',
+    priceCents: '21',
+    TotalPrice: '5.799,21',
+    link: 'https://produto.mercadolivre.com.br/mlb-2182066444-notebook-nitro-5-an517-52-50rs-i5-w11-512gb-gtx-1650-173'
+  },
+  {
+    index: 26,
+    title: "Notebook Gamer Nitro 5 An515-44-r5yz R5 16gb 512gb Ssd 15,6'",
+    priceAmount: '6.499',
+    priceCents: '08',
+    TotalPrice: '6.499,08',
+    link: 'https://produto.mercadolivre.com.br/mlb-1983207032-notebook-gamer-nitro-5-an515-44-r5yz-r5-16gb-512gb-ssd-156'
+  },
+  {
+    index: 27,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN515-44 preta 15.6", AMD Ryzen 5 4600H 8GB de RAM 512GB SSD, NVIDIA GeForce GTX 1650 144 Hz 1920x1080px Windows 10 Home',
+    priceAmount: '5.790',
+    TotalPrice: '5.790,null',
+    seller: 'ALISSON_EXPRESS',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an515-44-preta-156-amd-ryzen-5-4600h-8gb-de-ram-512gb-ssd-nvidia-geforce-gtx-1650-144-hz-1920x1080px-windows-10-home/p/mlb18563793?pdp_filters=category:mlb1652#searchvariation=mlb18563793&position=9&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 28,
+    title: "Notebook Nitro 5 An517-52-77kz I7 W11 512gb Gtx 1650 17.3'",
+    priceAmount: '7.999',
+    priceCents: '50',
+    TotalPrice: '7.999,50',
+    link: 'https://produto.mercadolivre.com.br/mlb-2179734050-notebook-nitro-5-an517-52-77kz-i7-w11-512gb-gtx-1650-173'
+  },
+  {
+    index: 29,
+    title: 'Notebook Gamer Acer Nitro 5 8gb 512gb Intel Core I5-10300h',
+    priceAmount: '4.936',
+    priceCents: '70',
+    TotalPrice: '4.936,70',
+    link: 'https://produto.mercadolivre.com.br/mlb-2616124134-notebook-gamer-acer-nitro-5-8gb-512gb-intel-core-i5-10300h'
+  },
+  {
+    index: 30,
+    title: "Notebook Acer Nitro 5 An515-45-r1fq R7 512gb Gtx 1650 15.6'",
+    priceAmount: '6.499',
+    priceCents: '65',
+    TotalPrice: '6.499,65',
+    link: 'https://produto.mercadolivre.com.br/mlb-2759394053-notebook-acer-nitro-5-an515-45-r1fq-r7-512gb-gtx-1650-156'
+  },
+  {
+    index: 31,
+    title: 'Nitro 5 Ryzen 7 - 4800hz - 512ssd - Tela 144hz - 16gb Ram',
+    priceAmount: '7.490',
+    priceCents: '30',
+    TotalPrice: '7.490,30',
+    link: 'https://produto.mercadolivre.com.br/mlb-2177025673-nitro-5-ryzen-7-4800hz-512ssd-tela-144hz-16gb-ram'
+  },
+  {
+    index: 32,
+    title: 'Notebook Nitro 5 Acer 17,3 I7 11800h 8gb 512gb Rtx3050 W11',
+    priceAmount: '6.599',
+    priceCents: '77',
+    TotalPrice: '6.599,77',
+    link: 'https://produto.mercadolivre.com.br/mlb-2814505523-notebook-nitro-5-acer-173-i7-11800h-8gb-512gb-rtx3050-w11'
+  },
+  {
+    index: 33,
+    title: 'Notebook Gamer Nitro 5 An515-44-r4ka R7 8gb 512gb Gtx 1650',
+    priceAmount: '6.999',
+    priceCents: '90',
+    TotalPrice: '6.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2623845400-notebook-gamer-nitro-5-an515-44-r4ka-r7-8gb-512gb-gtx-1650'
+  },
+  {
+    index: 34,
+    title: 'Notebook Gamer An515-55-79x0 Nitro 5 Intel Core I Acer',
+    priceAmount: '5.497',
+    priceCents: '80',
+    TotalPrice: '5.497,80',
+    link: 'https://produto.mercadolivre.com.br/mlb-2641921973-notebook-gamer-an515-55-79x0-nitro-5-intel-core-i-acer'
+  },
+  {
+    index: 35,
+    title: 'Notebook gamer Acer Aspire Nitro 5 AN515-54 preta e vermelha 15.6", Intel Core i5 9300H 8GB de RAM 1TB HDD 128GB SSD, NVIDIA GeForce GTX 1650 60 Hz 1920x1080px Linux Endless',    
+    priceAmount: '6.490',
+    priceCents: '21',
+    TotalPrice: '6.490,21',
+    seller: 'FBPARIS',
+    link: 'https://www.mercadolivre.com.br/notebook-gamer-acer-aspire-nitro-5-an515-54-preta-e-vermelha-156-intel-core-i5-9300h-8gb-de-ram-1tb-hdd-128gb-ssd-nvidia-geforce-gtx-1650-60-hz-1920x1080px-linux-endless/p/mlb15793893?pdp_filters=category:mlb1652#searchvariation=mlb15793893&position=10&search_layout=grid&type=product&tracking_id=c9015a4f-8019-424b-93a5-3f8481648949'
+  },
+  {
+    index: 36,
+    title: 'Notebook Gamer 8gb 1tb+256ssd R5 3550h An515-43-r9k7 Acer',
+    priceAmount: '6.479',
+    priceCents: '09',
+    TotalPrice: '6.479,09',
+    link: 'https://produto.mercadolivre.com.br/mlb-2608891429-notebook-gamer-8gb-1tb256ssd-r5-3550h-an515-43-r9k7-acer'
+  },
+  {
+    index: 37,
+    title: 'Notebook Acer Nitro 5 Ryzen 5 4600h - 8gb - 512 Ssd - 144hz',
+    priceAmount: '5.790',
+    priceCents: '05',
+    TotalPrice: '5.790,05',
+    link: 'https://produto.mercadolivre.com.br/mlb-2129576654-notebook-acer-nitro-5-ryzen-5-4600h-8gb-512-ssd-144hz'
+  },
+  {
+    index: 38,
+    title: 'Notebook Acer Vero Av15- 51-53ap I5 8gb 256gb Ssd 15.6 Eco',
+    priceAmount: '4.999',
+    priceCents: '90',
+    TotalPrice: '4.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2863633046-notebook-acer-vero-av15-51-53ap-i5-8gb-256gb-ssd-156-eco'
+  },
+  {
+    index: 39,
+    title: 'Notebook Gamer Acer Nitro An515-44-r54q Amd R5 8gb 512gb Ssd',
+    priceAmount: '7.999',
+    priceCents: '50',
+    TotalPrice: '7.999,50',
+    link: 'https://produto.mercadolivre.com.br/mlb-1968954113-notebook-gamer-acer-nitro-an515-44-r54q-amd-r5-8gb-512gb-ssd'
+  },
+  {
+    index: 40,
+    title: 'Notebook Acer Swift 3 Sf314-511-56ur Evo I5 16gb 512gb Ssd',
+    priceAmount: '8.999',
+    priceCents: '90',
+    TotalPrice: '8.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2869079037-notebook-acer-swift-3-sf314-511-56ur-evo-i5-16gb-512gb-ssd'
+  },
+  {
+    index: 41,
+    title: 'Notebook Acer Spin 3 Touch Sp314-55n-53eu I5 8gb 512gb Ssd',
+    priceAmount: '7.999',
+    priceCents: '50',
+    TotalPrice: '7.999,50',
+    link: 'https://produto.mercadolivre.com.br/mlb-2864780179-notebook-acer-spin-3-touch-sp314-55n-53eu-i5-8gb-512gb-ssd'
+  },
+  {
+    index: 42,
+    title: 'Notebook Acer Swift 3 Sf314-511-55ck Evo I5 8gb 512gb Ssd',
+    priceAmount: '8.499',
+    priceCents: '90',
+    TotalPrice: '8.499,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2869082986-notebook-acer-swift-3-sf314-511-55ck-evo-i5-8gb-512gb-ssd'
+  },
+  {
+    index: 43,
+    title: 'Notebook Acer Vero Av15-51-58zm I5 8gb 512gb Ssd 15.6 Eco',
+    priceAmount: '5.099',
+    priceCents: '90',
+    TotalPrice: '5.099,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2864180786-notebook-acer-vero-av15-51-58zm-i5-8gb-512gb-ssd-156-eco'
+  },
+  {
+    index: 44,
+    title: "Notebook Gamer Nitro 5 An517-54-55t5 I5 8gb 512gb Ssd 17.3'",
+    priceAmount: '5.999',
+    priceCents: '90',
+    TotalPrice: '5.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2812974026-notebook-gamer-nitro-5-an517-54-55t5-i5-8gb-512gb-ssd-173'
+  },
+  {
+    index: 45,
+    title: 'Notebook Acer Swift 3 Sf314-511-58k4 Evo I5 8gb 512gb Ssd',
+    priceAmount: '8.999',
+    priceCents: '90',
+    TotalPrice: '8.999,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2868828888-notebook-acer-swift-3-sf314-511-58k4-evo-i5-8gb-512gb-ssd'
+  },
+  {
+    index: 46,
+    title: "Notebook Gamer Nitro 5 An517-54-55t5 I5 8gb 512gb Ssd 17.3'",
+    priceAmount: '5.999',
+    priceCents: '87',
+    TotalPrice: '5.999,87',
+    link: 'https://produto.mercadolivre.com.br/mlb-2812974108-notebook-gamer-nitro-5-an517-54-55t5-i5-8gb-512gb-ssd-173'
+  },
+  {
+    index: 47,
+    title: 'Notebook Gamer An515-44-r4ka Nitro 5 Amd Ryzen 7 Acer',
+    priceAmount: '5.722',
+    priceCents: '20',
+    TotalPrice: '5.722,20',
+    link: 'https://produto.mercadolivre.com.br/mlb-2641876842-notebook-gamer-an515-44-r4ka-nitro-5-amd-ryzen-7-acer'
+  },
+  {
+    index: 48,
+    title: 'Notebook Gamer Nitro 5 I5 10300h 256gb W10h 8gb Gtx 1650 4gb',
+    priceAmount: '7.299',
+    priceCents: '01',
+    TotalPrice: '7.299,01',
+    link: 'https://produto.mercadolivre.com.br/mlb-2701731788-notebook-gamer-nitro-5-i5-10300h-256gb-w10h-8gb-gtx-1650-4gb'
+  }
+]
