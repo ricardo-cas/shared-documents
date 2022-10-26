9f27c51d021533be6c13c0d385cc4985a7912b4c
Ricardo Costa Alves dos Santos <65743471+ricardo-cas@users.noreply.github.com>
2022-10-25 22:46:11 -0300

    Create extracao-ml-20221025.js

---
+++ extracao-ml-20221025.js
@@ -0,0 +1,386 @@
+[
+  {
+    index: 0,
+    title: 'Coleira Peitoral Para Cachorro Pet Colete Refletivo + Guia',
+    priceAmount: '35',
+    priceCents: '99',
+    TotalPrice: '35,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2742048482-coleira-peitoral-para-cachorro-pet-colete-refletivo-guia'
+  },
+  {
+    index: 1,
+    title: 'Peitoral Coleira Com Regulagem Guia Para Cães Com Bolso',
+    priceAmount: '58',
+    priceCents: '45',
+    TotalPrice: '58,45',
+    link: 'https://produto.mercadolivre.com.br/mlb-1668152256-peitoral-coleira-com-regulagem-guia-para-ces-com-bolso'  
+  },
+  {
+    index: 2,
+    title: 'Coleira Peitoral Para Cachorro Pet Colete Refletivo + Guia',
+    priceAmount: '35',
+    priceCents: '99',
+    TotalPrice: '35,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2078788297-coleira-peitoral-para-cachorro-pet-colete-refletivo-guia'
+  },
+  {
+    index: 3,
+    title: 'Coleira Peitoral Com Bolso Com Regulagem Guia Para Caes',
+    priceAmount: '58',
+    priceCents: '79',
+    TotalPrice: '58,79',
+    link: 'https://produto.mercadolivre.com.br/mlb-1656760269-coleira-peitoral-com-bolso-com-regulagem-guia-para-caes'
+  },
+  {
+    index: 4,
+    title: 'Guia Para Cães Coleira Peitoral Com Alça Reforçado Pit Bull',
+    priceAmount: '49',
+    priceCents: '99',
+    TotalPrice: '49,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2196572812-guia-para-ces-coleira-peitoral-com-alca-reforcado-pit-bull'
+  },
+  {
+    index: 5,
+    title: 'Coleira Para Cachorro Peitoral Guia Pitbull Golden',
+    priceAmount: '34',
+    priceCents: '40',
+    TotalPrice: '34,40',
+    link: 'https://produto.mercadolivre.com.br/mlb-2063354346-coleira-para-cachorro-peitoral-guia-pitbull-golden'
+  },
+  {
+    index: 6,
+    title: 'Coleira Peitoral Guia Para Cachorros Pequenos Cães Gato Pet',
+    priceAmount: '33',
+    priceCents: '35',
+    TotalPrice: '33,35',
+    link: 'https://produto.mercadolivre.com.br/mlb-2114592285-coleira-peitoral-guia-para-cachorros-pequenos-ces-gato-pet'
+  },
+  {
+    index: 7,
+    title: 'Coleira Para Cachorro Peitoral Guia Pitbull Golden Uzy Pet',
+    priceAmount: '34',
+    priceCents: '40',
+    TotalPrice: '34,40',
+    link: 'https://produto.mercadolivre.com.br/mlb-2193512790-coleira-para-cachorro-peitoral-guia-pitbull-golden-uzy-pet'
+  },
+  {
+    index: 8,
+    title: 'Coleira Peitoral Cachorro Guia Doberman Pitbull Anti Puxao',
+    priceAmount: '75',
+    priceCents: '25',
+    TotalPrice: '75,25',
+    link: 'https://produto.mercadolivre.com.br/mlb-854116544-coleira-peitoral-cachorro-guia-doberman-pitbull-anti-puxao'
+  },
+  {
+    index: 9,
+    title: 'Coleira Peitoral Para Cachorro Pet Colete Refletivo + Guia',
+    priceAmount: '27',
+    priceCents: '59',
+    TotalPrice: '27,59',
+    link: 'https://produto.mercadolivre.com.br/mlb-2838538495-coleira-peitoral-para-cachorro-pet-colete-refletivo-guia'
+  },
+  {
+    index: 10,
+    title: 'Coleira Para Cachorro Peitoral Guia Shitzu Pug Pq - Md Porte',
+    priceAmount: '32',
+    priceCents: '40',
+    TotalPrice: '32,40',
+    link: 'https://produto.mercadolivre.com.br/mlb-2644469667-coleira-para-cachorro-peitoral-guia-shitzu-pug-pq-md-porte'
+  },
+  {
+    index: 11,
+    title: 'Coleira Peitoral Cães Pequenos Filhotes Pinscher Shin Tzu',
+    priceAmount: '37',
+    priceCents: '99',
+    TotalPrice: '37,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2141734803-coleira-peitoral-ces-pequenos-filhotes-pinscher-shin-tzu'
+  },
+  {
+    index: 12,
+    title: 'Coleira Peitoral Cães E Gatos Ajustável Com Guia Colete Pet',
+    priceAmount: '37',
+    priceCents: '99',
+    TotalPrice: '37,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2078814312-coleira-peitoral-ces-e-gatos-ajustavel-com-guia-colete-pet'
+  },
+  {
+    index: 13,
+    title: 'Coleira Peitoral Para Cachorro Coleira Com Guia Promoção',
+    priceAmount: '54',
+    priceCents: '63',
+    TotalPrice: '54,63',
+    link: 'https://produto.mercadolivre.com.br/mlb-1652243102-coleira-peitoral-para-cachorro-coleira-com-guia-promoco'
+  },
+  {
+    index: 14,
+    title: 'Coleira Peitoral Com Bolso + Cinto De Seguranca Para Carro',
+    priceAmount: '71',
+    priceCents: '38',
+    TotalPrice: '71,38',
+    link: 'https://produto.mercadolivre.com.br/mlb-1784400004-coleira-peitoral-com-bolso-cinto-de-seguranca-para-carro'
+  },
+  {
+    index: 15,
+    title: 'Peitoral Antipuxão Zeedog Gotham Para Cachorros',
+    priceAmount: '134',
+    priceCents: '80',
+    TotalPrice: '134,80',
+    link: 'https://produto.mercadolivre.com.br/mlb-1767991229-peitoral-antipuxo-zeedog-gotham-para-cachorros'
+  },
+  {
+    index: 16,
+    title: 'Coleira Peitoral Com Guia Ajustável Para Cães E Gatos Oferta',
+    priceAmount: '35',
+    priceCents: '99',
+    TotalPrice: '35,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2078781839-coleira-peitoral-com-guia-ajustavel-para-ces-e-gatos-oferta'
+  },
+  {
+    index: 17,
+    title: 'Coleira Peitoral Para Cachorro Pet Colete Com Guia Promoção',
+    priceAmount: '38',
+    priceCents: '66',
+    TotalPrice: '38,66',
+    link: 'https://produto.mercadolivre.com.br/mlb-1652228436-coleira-peitoral-para-cachorro-pet-colete-com-guia-promoco'
+  },
+  {
+    index: 18,
+    title: 'Coleira Peitoral Cachorro Porte Pequeno E Medio',
+    priceAmount: '46',
+    priceCents: '45',
+    TotalPrice: '46,45',
+    link: 'https://produto.mercadolivre.com.br/mlb-1123748021-coleira-peitoral-cachorro-porte-pequeno-e-medio'
+  },
+  {
+    index: 19,
+    title: 'Coleira Peitoral Guia Para Cachorros Gato Pequenos Pet Luxo',
+    priceAmount: '35',
+    priceCents: '99',
+    TotalPrice: '35,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2078814088-coleira-peitoral-guia-para-cachorros-gato-pequenos-pet-luxo'
+  },
+  {
+    index: 20,
+    title: 'Kit Peitoral - Coleira - Guia - Cinto De Segurança + Brinde',
+    priceAmount: '99',
+    priceCents: '90',
+    TotalPrice: '99,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-1348891318-kit-peitoral-coleira-guia-cinto-de-seguranca-brinde'
+  },
+  {
+    index: 21,
+    title: 'Coleira Peitoral E Guia Cachorro Médio Grande Regulável',
+    priceAmount: '49',
+    priceCents: '99',
+    TotalPrice: '49,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-1107023328-coleira-peitoral-e-guia-cachorro-medio-grande-regulavel'
+  },
+  {
+    index: 22,
+    title: 'Coleira Peitoral Para Cachorro Gato Uzy Pet Colete Com Guia',
+    priceAmount: '31',
+    priceCents: '90',
+    TotalPrice: '31,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2630725495-coleira-peitoral-para-cachorro-gato-uzy-pet-colete-com-guia'
+  },
+  {
+    index: 23,
+    title: 'Peitoral Security Police Para Cães (coleira Pet Shop)',
+    priceAmount: '46',
+    priceCents: '99',
+    TotalPrice: '46,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-843797689-peitoral-security-police-para-ces-coleira-pet-shop'
+  },
+  {
+    index: 24,
+    title: 'Coleira Cachorro Peitoral Guia+enforcador Corda Guia Uzypet',
+    priceAmount: '59',
+    priceCents: '90',
+    TotalPrice: '59,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2205863536-coleira-cachorro-peitoral-guiaenforcador-corda-guia-uzypet'
+  },
+  {
+    index: 25,
+    title: 'Coleira Para Cachorro Peitoral Guia Pitbull Labrador',
+    priceAmount: '34',
+    priceCents: '40',
+    TotalPrice: '34,40',
+    link: 'https://produto.mercadolivre.com.br/mlb-2135042760-coleira-para-cachorro-peitoral-guia-pitbull-labrador'
+  },
+  {
+    index: 26,
+    title: 'Coleira Cachorro Peitoral Yorkshire Lhasa Shitsu Terrier Pet',
+    priceAmount: '31',
+    priceCents: '99',
+    TotalPrice: '31,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2630810224-coleira-cachorro-peitoral-yorkshire-lhasa-shitsu-terrier-pet'
+  },
+  {
+    index: 27,
+    title: 'Guia Para Cachorros Filhotes Cães Gatos Pet Coleira Peitoral',
+    priceAmount: '29',
+    priceCents: '99',
+    TotalPrice: '29,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2747855216-guia-para-cachorros-filhotes-ces-gatos-pet-coleira-peitoral'
+  },
+  {
+    index: 28,
+    title: 'Coleira Peitoral Para Cachorro Pet Colete + Guia',
+    priceAmount: '23',
+    priceCents: '90',
+    TotalPrice: '23,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2214231352-coleira-peitoral-para-cachorro-pet-colete-guia'
+  },
+  {
+    index: 29,
+    title: 'Coleira Anti Puxão Peitoral Cão Porte Pequeno Médio Grande',
+    priceAmount: '38',
+    priceCents: '90',
+    TotalPrice: '38,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2059716212-coleira-anti-puxo-peitoral-co-porte-pequeno-medio-grande'
+  },
+  {
+    index: 30,
+    title: 'Coleira Colete Com Guia Para Cães Peitoral Super Promoção',
+    priceAmount: '49',
+    priceCents: '68',
+    TotalPrice: '49,68',
+    link: 'https://produto.mercadolivre.com.br/mlb-1649435839-coleira-colete-com-guia-para-ces-peitoral-super-promoco'
+  },
+  {
+    index: 31,
+    title: 'Coleira Cachorro Peitoral Completa Pitbull, Labrador, Boxer',
+    priceAmount: '70',
+    priceCents: '99',
+    TotalPrice: '70,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-812053092-coleira-cachorro-peitoral-completa-pitbull-labrador-boxer'
+  },
+  {
+    index: 32,
+    title: 'Peitoral Coleira Guia Canil C Regulagem X Para Cachorro Cão',
+    priceAmount: '53',
+    priceCents: '96',
+    TotalPrice: '53,96',
+    link: 'https://produto.mercadolivre.com.br/mlb-1860234600-peitoral-coleira-guia-canil-c-regulagem-x-para-cachorro-co'
+  },
+  {
+    index: 33,
+    title: 'Coleira Peitoral Cachorros Cães E Gatos Filhote Shitzu Lulu',
+    priceAmount: '31',
+    priceCents: '99',
+    TotalPrice: '31,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2747818520-coleira-peitoral-cachorros-ces-e-gatos-filhote-shitzu-lulu'
+  },
+  {
+    index: 34,
+    title: 'Coleira Peitoral Guia Para Cachorros Gato Pequenos Pet Luxo',
+    priceAmount: '38',
+    priceCents: '97',
+    TotalPrice: '38,97',
+    link: 'https://produto.mercadolivre.com.br/mlb-2103757116-coleira-peitoral-guia-para-cachorros-gato-pequenos-pet-luxo'
+  },
+  {
+    index: 35,
+    title: 'Peitoral Antipuxão Zeedog Fatboy Para Cachorros',
+    priceAmount: '134',
+    priceCents: '80',
+    TotalPrice: '134,80',
+    link: 'https://produto.mercadolivre.com.br/mlb-1848334905-peitoral-antipuxo-zeedog-fatboy-para-cachorros'
+  },
+  {
+    index: 36,
+    title: 'Guia + Coleira Peitoral Cães Cachorro Porte Pequeno E Medio',
+    priceAmount: '31',
+    priceCents: '99',
+    TotalPrice: '31,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2141780074-guia-coleira-peitoral-ces-cachorro-porte-pequeno-e-medio'
+  },
+  {
+    index: 37,
+    title: 'Colete Com Guia Ajustável Para Cães E Gatos Coleira Peitoral',
+    priceAmount: '31',
+    priceCents: '99',
+    TotalPrice: '31,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2743537274-colete-com-guia-ajustavel-para-ces-e-gatos-coleira-peitoral'
+  },
+  {
+    index: 38,
+    title: 'Coleira Colete Peitoral Ajustável C/guia -feito No Brasil',
+    priceAmount: '32',
+    priceCents: '99',
+    TotalPrice: '32,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-2669098054-coleira-colete-peitoral-ajustavel-cguia-feito-no-brasil'
+  },
+  {
+    index: 39,
+    title: 'Coleira Peitoral Modelo H Com Guia Para Cães',
+    priceAmount: '41',
+    priceCents: '10',
+    TotalPrice: '41,10',
+    link: 'https://produto.mercadolivre.com.br/mlb-2040642468-coleira-peitoral-modelo-h-com-guia-para-ces'
+  },
+  {
+    index: 40,
+    title: 'Kit Peitoral - Guia - Brinde Plaquinha De Identificação',
+    priceAmount: '59',
+    priceCents: '90',
+    TotalPrice: '59,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-1348525949-kit-peitoral-guia-brinde-plaquinha-de-identificaco'
+  },
+  {
+    index: 41,
+    title: 'Peitoral Para Cães Flyharness Zee.dog Gotham Tam 3',
+    priceAmount: '201',
+    priceCents: '50',
+    TotalPrice: '201,50',
+    link: 'https://produto.mercadolivre.com.br/mlb-1598513423-peitoral-para-ces-flyharness-zeedog-gotham-tam-3'
+  },
+  {
+    index: 42,
+    title: 'Kit 12 Peitoral Guia Tradicional Cachorro Petshop Atacado',
+    priceAmount: '119',
+    priceCents: '99',
+    TotalPrice: '119,99',
+    link: 'https://produto.mercadolivre.com.br/mlb-1663942738-kit-12-peitoral-guia-tradicional-cachorro-petshop-atacado'
+  },
+  {
+    index: 43,
+    title: 'Coleira Peitoral Colete Guia Para Cães + Cinto De Segurança',
+    priceAmount: '42',
+    priceCents: '90',
+    TotalPrice: '42,90',
+    link: 'https://produto.mercadolivre.com.br/mlb-2741572317-coleira-peitoral-colete-guia-para-ces-cinto-de-seguranca'
+  },
+  {
+    index: 44,
+    title: 'Peitoral Antipuxão Zeedog Valley Para Cachorros',
+    priceAmount: '134',
+    priceCents: '80',
+    TotalPrice: '134,80',
+    link: 'https://produto.mercadolivre.com.br/mlb-2640594357-peitoral-antipuxo-zeedog-valley-para-cachorros'
+  },
+  {
+    index: 45,
+    title: 'Coleira Peitoral Cachorro Guia Boxer Labrador Pit Bull Mola',
+    priceAmount: '75',
+    priceCents: '25',
+    TotalPrice: '75,25',
+    link: 'https://produto.mercadolivre.com.br/mlb-815171842-coleira-peitoral-cachorro-guia-boxer-labrador-pit-bull-mola'
+  },
+  {
+    index: 46,
+    title: 'Coleira Peitoral Ajustável Para Cães Pequeno Médio Grande',
+    priceAmount: '38',
+    priceCents: '98',
+    TotalPrice: '38,98',
+    link: 'https://produto.mercadolivre.com.br/mlb-1661764303-coleira-peitoral-ajustavel-para-ces-pequeno-medio-grande'
+  },
+  {
+    index: 47,
+    title: 'Coleira Peitoral Ajustável Com Guia Para Cachorro E Gatos',
+    priceAmount: '38',
+    priceCents: '48',
+    TotalPrice: '38,48',
+    link: 'https://produto.mercadolivre.com.br/mlb-1722438876-coleira-peitoral-ajustavel-com-guia-para-cachorro-e-gatos'
+  }
+]
